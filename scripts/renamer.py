b91c2e13787858545b8be53bad66dfb51620d3dd
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-03-30 23:21:01 -0400

    Added renamer.py script.

---
+++ scripts/renamer.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+import os
+
+def main():
+    i = 0
+#   Insert the path to the source folder here.
+    path = "~/source/folder/for/the/files/"
+    for filename in os.listdir(path):
+        destination = "img" + str(i) + ".jpg"
+        source = path + filename
+        destination = path + destination
+        os.rename(source, destination)
+        i += 1
+
+if __name__ == '__main__':
+    main()
