1e667a1233a39b42527cfa78602dfd0cf3f09f22
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-04-04 15:19:49 -0400

    Added password_generator.py.

--- scripts/password_generator.py
+++ scripts/password_generator.py
@@ -5,3 +5,6 @@ import random
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*().,?'
 
 number = input('Amount of passwords of generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
