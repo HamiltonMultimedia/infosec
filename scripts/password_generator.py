7ade22c93382e650c1d0b46034c6d21647b87e09
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-04-04 15:21:02 -0400

    Added password_generator.py.

--- scripts/password_generator.py
+++ scripts/password_generator.py
@@ -8,3 +8,6 @@ number = input('Amount of passwords of generate: ')
 number = int(number)
 
 length = input('Input your password length: ')
+length = int(length)
+
+print('\nHere are your passwords: ')
