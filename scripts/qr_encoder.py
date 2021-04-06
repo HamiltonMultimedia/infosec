61e98367ee5d6be3a55a103c6c957ed3d58e98b8
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-04-05 21:55:21 -0400

    Added qr_encoder.py.

--- scripts/qr_encoder.py
+++ scripts/qr_encoder.py
@@ -7,4 +7,8 @@
  Uncomment from line 3 on, after installing qrcode.
 """
 
+data = input('Enter your message to be encoded here: ')
 
+# qr = qrcode.QRCode(version = 1, box_size=10, border=5)
+
+# qr.add_data(data)
