28f835e0d7ab808d3cdb7f6abfcab738428828d1
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2021-04-05 22:00:30 -0400

    Added qr_decoder.py.

--- scripts/qr_decoder.py
+++ scripts/qr_decoder.py
@@ -1,3 +1,9 @@
 #!/usr/local/bin/python3
 
-from pyzbar.pyzbar import decode
+# from pyzbar.pyzbar import decode
+# from PIL import Image
+
+"""
+ Remember to pip install pyzbar before running this.
+"""
+
